12a44cdadd9f4bffacb96132d4dee07bfb1227e7
KiranGaikwad2020 <59598642+KiranGaikwad2020@users.noreply.github.com>
2026-08-12 09:02:33 +0530

    Include setup steps for running the program
    Added installation instructions for required packages.

--- Artificial_Intelligence_Lab/AILab_Programs/Experi_2_DecesionExpertSystem/ModifiedExperiment2.py
+++ Artificial_Intelligence_Lab/AILab_Programs/Experi_2_DecesionExpertSystem/ModifiedExperiment2.py
@@ -36,3 +36,9 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+# Execute the following steps to execute the program
+# 1. $ sudo apt update
+# 2. $ sudo apt install python3-pip
+# 3. $ python3 -m pip install experta
+# 4. $ python3 -m pip install frozendict==2.3.4
